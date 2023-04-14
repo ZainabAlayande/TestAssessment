631da4363002188ccc6e069d01f0ebfa94dd0114
The_Zen_Herself <117924296+ZainabAlayande@users.noreply.github.com>
2023-04-14 15:38:26 +0100

    java assessment

---
+++ Semicolon.java
@@ -0,0 +1,11 @@
+public class Semicolon {
+
+    public static void main(String[] args) {
+
+        String word = "SEMICOLON";
+
+        for (int i = 0; i < word.length(); i++) {
+            System.out.println(word.charAt(i));
+        }
+    }
+}
